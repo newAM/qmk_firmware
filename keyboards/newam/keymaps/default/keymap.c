e884fc45e9731b04f630b57620bb7eee150fa5de
Alex Martens <alex@thinglab.org>
2023-07-14 21:00:42 -0700

    Fix keymaps

--- keyboards/newam/keymaps/default/keymap.c
+++ keyboards/newam/keymaps/default/keymap.c
@@ -31,12 +31,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                              └───┴───┴───┘ └───┴───┴───┘
      */
     [LAY_D] = LAYOUT(
-        KC_ESC,  KC_F1,          KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,       KC_F12,  KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,            KC_F11,
-        KC_GRV,  KC_1,           KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,       KC_SYRQ, KC_6,  KC_7,  KC_8,    KC_9,    KC_0,              KC_MINS,
-        KC_TAB,  KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,       KC_NO,   KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,              KC_BSLS,
-        KC_ESC,  KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,       KC_EQL,  KC_H,  KC_J,  KC_K,    KC_L,    LT(LAY_N,KC_SCLN), KC_QUOT,
-        KC_LSFT, LT(LAY_N,KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,       KC_NO,   KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT,
-        KC_LCTL, KC_LGUI,        KC_LALT, KC_LEFT, KC_RGHT, KC_0,    KC_1,        KC_2,    KC_3,  KC_UP, KC_DOWN, KC_VOLU, KC_VOLD,           MO(LAY_F),
+        KC_ESC,  KC_F1,          KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,       KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,     KC_F10,            KC_F11,
+        KC_GRV,  KC_1,           KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,       KC_SYRQ, KC_6,    KC_7,    KC_8,    KC_9,      KC_0,              KC_MINS,
+        KC_TAB,  KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,       KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,              KC_BSLS,
+        KC_ESC,  KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,       KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,      LT(LAY_N,KC_SCLN), KC_QUOT,
+        KC_LSFT, LT(LAY_N,KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,       KC_NO,   KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH,           KC_RSFT,
+        KC_LCTL, KC_LGUI,        KC_LALT, KC_LEFT, KC_RGHT, KC_0,    KC_1,        KC_UP,   KC_DOWN, KC_VOLU, KC_VOLD, MO(LAY_F), KC_2,              KC_3,
         KC_4,    KC_5,           KC_6,
         KC_7,    KC_8,           KC_9
     ),
@@ -44,10 +44,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAY_N] = LAYOUT(
         _______, _______,  _______,  _______,  _______, _______, _______,    _______, _______,  _______,  _______, _______, _______, _______,
         _______, _______,  _______,  _______,  _______, _______, _______,    _______, _______,  _______,  _______, _______, _______, _______,
-        _______, _______,  KC_AMPR,  KC_LCBR,  KC_RCBR, _______, _______,    _______, _______,  KC_KP_7,  KC_KP_8, KC_KP_9, _______, _______,
-        _______, _______,  KC_ASTR,  KC_LPRN,  KC_RPRN, _______, _______,    _______, _______,  KC_KP_4,  KC_KP_5, KC_KP_6, _______, _______,
-        _______, _______,  _______,  KC_LBRC,  KC_RBRC, _______, _______,    _______, _______,  KC_KP_1,  KC_KP_2, KC_KP_3, _______, _______,
-        _______, _______,  _______,  _______,  _______, _______, _______,    _______, _______,  KC_KP_0,  KC_PDOT, KC_KP_0, KC_PEQL, _______,
+        _______, _______,  KC_AMPR,  KC_LCBR,  KC_RCBR, _______, _______,    _______, _______,  KC_7,     KC_8,    KC_9,    _______, _______,
+        _______, _______,  KC_ASTR,  KC_LPRN,  KC_RPRN, _______, _______,    _______, _______,  KC_4,     KC_5,    KC_6,    _______, _______,
+        _______, _______,  _______,  KC_LBRC,  KC_RBRC, _______, _______,    _______, _______,  KC_1,     KC_2,    KC_3,    _______, _______,
+        _______, _______,  _______,  _______,  _______, _______, _______,    _______, _______,  KC_0,     KC_DOT,  KC_0,    KC_EQL, _______,
         _______, _______,  _______,
         _______, _______,  _______
     )
